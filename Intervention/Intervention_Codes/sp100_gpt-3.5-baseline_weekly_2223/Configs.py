c0bb887f1b050f200bd595a833cd4f85e79f3e0a
Machine-Behaviorism <pnas2024_ustc@163.com>
2024-09-02 08:41:51 +0800

    Add Intervention folder

---
+++ Intervention/Intervention_Codes/sp100_gpt-3.5-baseline_weekly_2223/Configs.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+import os
+
+OPENAI_ORGANIZATION = "org-TfIH2DbMdVVsDTC4wawLqvcv"
+API_KEY = "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"
+COMPLETION_MODEL =  "gpt-3.5-turbo-16k"
+# COMPLETION_MODEL = "gpt-4" 
+# COMPLETION_MODEL ="gpt-4-1106-preview"
+# COMPLETION_MODEL ="gpt-4-0613"
+# COMPLETION_MODEL ="gpt-3.5-turbo"
+# COMPLETION_MODEL ="gpt-3.5-turbo-1106"
+# COMPLETION_MODEL = "gpt-3.5-turbo-0613"
+
+# Root directory of Dataset
+source_data_path ="/root/PNAS_Github/Dataset/sp_100_stocks"
+Current_Input_data_path = "./sp100_Input_Dataset"
+
+root_path = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir,os.pardir,os.pardir))
+Results_path = os.path.abspath(os.path.join(root_path, 'Intervention/Intervention_Results', "sp100_gpt-3.5-baseline_weekly_2223"))
+# print("Results_path", Results_path)
+
